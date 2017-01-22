2980c01b31050fc9805d6588262a853e6a3d45ea
ivo <ivonindza@gmail.com>
2017-01-22 20:07:30 +0100

    Update settings

--- benchmarks/mahout/0.2.12/files/benchmark.sh
+++ benchmarks/mahout/0.2.12/files/benchmark.sh
@@ -1,40 +1,19 @@
 #!/bin/bash
 
-DATASET_SIZE=${1:-small}
-DATASET=wiki-${DATASET_SIZE}
-URL=
-
 RED='\033[0;31m'
 RESET='\033[0m'
 
-case $DATASET_SIZE in
-  small)
-    URL=https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles1.xml-p000000010p000030302.bz2
-    ;;
-  medium)
-    URL=https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles10.xml-p002336425p003046511.bz2
-    ;;
-  full)
-    URL=https://dumps.wikimedia.org/enwiki/latest/enwiki-latest-pages-articles.xml.bz2
-    ;;
-esac
-
-if [ ! -f /root/$DATASET ]; then
-  curl -o /root/${DATASET}.bz2 $URL
-  bunzip2 /root/${DATASET}.bz2
-fi
-
-${HADOOP_PREFIX}/bin/hdfs dfs -test -e /user/root/$DATASET
+${HADOOP_PREFIX}/bin/hdfs dfs -test -e /user/root/wiki
 if [ $? -ne 0 ]; then
   ${HADOOP_PREFIX}/bin/hdfs dfs -mkdir -p /user/root
-  ${HADOOP_PREFIX}/bin/hdfs dfs -put /root/$DATASET /user/root/
+  ${HADOOP_PREFIX}/bin/hdfs dfs -put /root/wiki /user/root/
 fi
 
 START=$(($(date +"%s%N")/1000000))
 
 # Create sequence files from wiki
 echo -e "${RED}Mahout: seqwiki${RESET}"
-${MAHOUT_HOME}/bin/mahout seqwiki -c /root/categories -i /user/root/$DATASET -o /user/root/wiki-seq
+${MAHOUT_HOME}/bin/mahout seqwiki -c /root/categories -i /user/root/wiki -o /user/root/wiki-seq
 # Convert sequence files to vectors using bigrams
 echo -e "${RED}Mahout: seq2sparse${RESET}"
 ${MAHOUT_HOME}/bin/mahout seq2sparse -i /user/root/wiki-seq -o /user/root/wiki-vectors -lnorm -nv -wt tfidf -ow -ng 2
